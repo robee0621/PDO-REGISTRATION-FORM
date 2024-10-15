0c930bad59c901f4a4bea9492a4aeec2ea7232d8
robee0621 <167277634+robee0621@users.noreply.github.com>
2024-10-15 22:01:21 +0800

    Delete dbConfig.php

--- dbConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "job_applications";
-$dsn = "mysql:host={$host};dbname={$dbname}";
-$pdo = new PDO($dsn, $user, $password);
-?>
